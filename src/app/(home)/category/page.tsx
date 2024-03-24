997a4d742b6259be2ffafb100bfefea71df36e33
Pi Pi <diwas118151@gmail.com>
2024-03-24 18:11:26 +0700

    attempt 4

--- src/app/(home)/category/page.tsx
+++ src/app/(home)/category/page.tsx
@@ -11,9 +11,9 @@ function CategoryPage() {
 
       {/* products */}
       <div className='grid grid-cols-1 gap-21 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+        {/* <ProductCard />
         <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        <ProductCard /> */}
       </div>
     </div>
   )
