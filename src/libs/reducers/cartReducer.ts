4c8352c90f3aa38724f2af1555c2c1af4b4df712
Pi Pi <diwas118151@gmail.com>
2024-03-26 21:30:27 +0700

    okela

--- src/libs/reducers/cartReducer.ts
+++ src/libs/reducers/cartReducer.ts
@@ -1,11 +1,22 @@
 import { FullyCartItem } from '@/app/api/cart/route'
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
+// Define a function to access localStorage safely
+const getLocalCartItems = () => {
+  if (typeof window !== 'undefined') {
+    // Access localStorage only if window (browser) is defined
+    return JSON.parse(localStorage.getItem('localCart') ?? '[]') as FullyCartItem[]
+  } else {
+    // Return empty array if running on server-side or in environments where localStorage is not available
+    return []
+  }
+}
+
 export const cart = createSlice({
   name: 'cart',
   initialState: {
     items: [] as FullyCartItem[],
-    localItems: JSON.parse(localStorage.getItem('localCart') ?? '[]') as FullyCartItem[],
+    localItems: getLocalCartItems(),
   },
   reducers: {
     // database cart
